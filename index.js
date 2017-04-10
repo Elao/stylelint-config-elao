63e3738be43492c9a4ded60e5b9bc82efe19bd25
Florian Rey <florian.rey@elao.com>
2017-04-10 13:32:55 +0200

    Remove "scss/dollar-variable-colon-newline-after" as it breaks sass map multiline syntax

--- index.js
+++ index.js
@@ -104,7 +104,6 @@ module.exports = {
     // Plugin - Scss
     // ==========================
     "scss/at-mixin-argumentless-call-parentheses": "always", // Require or disallow parentheses in argumentless @mixin calls.
-    "scss/dollar-variable-colon-newline-after": "always-multi-line", // Require a newline after the colon in $-variable declarations.
     "scss/dollar-variable-colon-space-after": "always", // Require a single space or disallow whitespace after the colon in $-variable declarations.
     "scss/dollar-variable-colon-space-before": "never", // Require a single space or disallow whitespace before the colon in $-variable declarations.
     //
